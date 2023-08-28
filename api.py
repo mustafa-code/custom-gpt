3c6de5cbb75a67f1cc3ed3066eca6476130af069
Mustafa El-sir <tommy80080@gmail.com>
2023-08-28 13:40:43 +0300

    Add terminal and api options

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 import requests
-import streamlit as st
+import os
+from flask import Flask, jsonify, request
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 from PyPDF2.errors import PdfReadError
@@ -17,12 +18,13 @@ from langchain.prompts import PromptTemplate
 def get_pdf_text(file_list):
     text = ""
     for file in file_list:
-        try:
-            pdf_reader = PdfReader(file)
-            for page in pdf_reader.pages:
-                text += page.extract_text()+"\n"
-        except PdfReadError:
-            text += file.read().decode("utf-8")+"\n"
+        # try:
+        #     pdf_reader = PdfReader(file)
+        #     for page in pdf_reader.pages:
+        #         text += page.extract_text()+"\n"
+        # except PdfReadError:
+        #     text += file.read().decode("utf-8")+"\n"
+        text += file.read()+"\n"
     return text
 
 
@@ -82,61 +84,40 @@ def get_conversation_chain(vectorstore):
     return conversation_chain
 
 
-def handle_userinput(user_question):
-    response = st.session_state.conversation(user_question)
 
-    st.session_state.chat_history.insert(0, response)
+app = Flask(__name__)
 
-    for i, message in enumerate(st.session_state.chat_history):
-        
-        upvoteUrl = "javascript:;"
-        downvoteUrl = "javascript:;"
-        result = message["result"]
-        botTemp = bot_template.replace("{{MSG}}", result)
-        botTemp = botTemp.replace("{{DOWNVOTE_URL}}", downvoteUrl)
-        botTemp = botTemp.replace("{{UPVOTE_URL}}", upvoteUrl)
-        # botTemp = botTemp.replace("{{SOURCE_DOCUMENT}}", message["source_documents"][0].page_content)
-        st.write(botTemp, unsafe_allow_html=True)
-
-        userTemp = user_template.replace("{{MSG}}", message["query"])
-        st.write(userTemp, unsafe_allow_html=True)
-
-def main():
+@app.route("/api/prompt_route", methods=["POST"])
+def prompt_route():
+    print("hello")
+    user_prompt = request.form.get("user_prompt")
     load_dotenv()
-    st.set_page_config(page_title="Chat with multiple PDFs",
-                       page_icon=":books:")
-    st.write(css, unsafe_allow_html=True)
+    # get pdf text
 
-    if "conversation" not in st.session_state:
-        st.session_state.conversation = None
-    if "chat_history" not in st.session_state:
-        st.session_state.chat_history = []
+    files = os.listdir("source_documents")
+    files_objects = []
+    for file in files:
+        file_object = open("source_documents/"+file, 'r')
+        files_objects.append(file_object)
 
-    st.header("Chat with multiple PDFs :books:")
-    user_question = st.text_input("Ask a question about your documents:")
-    if user_question:
-        handle_userinput(user_question)
+    # print(files_objects)
 
-    with st.sidebar:
-        st.subheader("Your documents")
-        pdf_docs = st.file_uploader(
-            "Upload your PDFs here and click on 'Process'", accept_multiple_files=True, type=["pdf", "txt"])
-        if st.button("Process"):
-            with st.spinner("Processing"):
-                # get pdf text
-                raw_text = get_pdf_text(pdf_docs)
+    raw_text = get_pdf_text(files_objects)
 
-                # get the text chunks
-                text_chunks = get_text_chunks(raw_text)
-                st.write("Chunks length: {number}".format(number=len(text_chunks)))
+    # get the text chunks
+    text_chunks = get_text_chunks(raw_text)
 
-                # create vector store
-                vectorstore = get_vectorstore(text_chunks)
+    # create vector store
+    vectorstore = get_vectorstore(text_chunks)
 
-                # create conversation chain
-                st.session_state.conversation = get_conversation_chain(
-                    vectorstore)
+    # create conversation chain
+    conversation = get_conversation_chain(vectorstore)
+
+    response = conversation(user_prompt)
+
+    return jsonify({"response": response})
 
 
 if __name__ == '__main__':
-    main()
+    app.run(debug=False, port=5110)
+    # main()
--- terminal.py
+++ terminal.py
@@ -1,5 +1,5 @@
 import requests
-import streamlit as st
+import os
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 from PyPDF2.errors import PdfReadError
@@ -17,12 +17,13 @@ from langchain.prompts import PromptTemplate
 def get_pdf_text(file_list):
     text = ""
     for file in file_list:
-        try:
-            pdf_reader = PdfReader(file)
-            for page in pdf_reader.pages:
-                text += page.extract_text()+"\n"
-        except PdfReadError:
-            text += file.read().decode("utf-8")+"\n"
+        # try:
+        #     pdf_reader = PdfReader(file)
+        #     for page in pdf_reader.pages:
+        #         text += page.extract_text()+"\n"
+        # except PdfReadError:
+        #     text += file.read().decode("utf-8")+"\n"
+        text += file.read()+"\n"
     return text
 
 
@@ -82,61 +83,42 @@ def get_conversation_chain(vectorstore):
     return conversation_chain
 
 
-def handle_userinput(user_question):
-    response = st.session_state.conversation(user_question)
-
-    st.session_state.chat_history.insert(0, response)
-
-    for i, message in enumerate(st.session_state.chat_history):
-        
-        upvoteUrl = "javascript:;"
-        downvoteUrl = "javascript:;"
-        result = message["result"]
-        botTemp = bot_template.replace("{{MSG}}", result)
-        botTemp = botTemp.replace("{{DOWNVOTE_URL}}", downvoteUrl)
-        botTemp = botTemp.replace("{{UPVOTE_URL}}", upvoteUrl)
-        # botTemp = botTemp.replace("{{SOURCE_DOCUMENT}}", message["source_documents"][0].page_content)
-        st.write(botTemp, unsafe_allow_html=True)
-
-        userTemp = user_template.replace("{{MSG}}", message["query"])
-        st.write(userTemp, unsafe_allow_html=True)
-
 def main():
     load_dotenv()
-    st.set_page_config(page_title="Chat with multiple PDFs",
-                       page_icon=":books:")
-    st.write(css, unsafe_allow_html=True)
+    # get pdf text
 
-    if "conversation" not in st.session_state:
-        st.session_state.conversation = None
-    if "chat_history" not in st.session_state:
-        st.session_state.chat_history = []
+    files = os.listdir("source_documents")
+    files_objects = []
+    for file in files:
+        file_object = open("source_documents/"+file, 'r')
+        files_objects.append(file_object)
 
-    st.header("Chat with multiple PDFs :books:")
-    user_question = st.text_input("Ask a question about your documents:")
-    if user_question:
-        handle_userinput(user_question)
+    # print(files_objects)
 
-    with st.sidebar:
-        st.subheader("Your documents")
-        pdf_docs = st.file_uploader(
-            "Upload your PDFs here and click on 'Process'", accept_multiple_files=True, type=["pdf", "txt"])
-        if st.button("Process"):
-            with st.spinner("Processing"):
-                # get pdf text
-                raw_text = get_pdf_text(pdf_docs)
+    raw_text = get_pdf_text(files_objects)
 
-                # get the text chunks
-                text_chunks = get_text_chunks(raw_text)
-                st.write("Chunks length: {number}".format(number=len(text_chunks)))
+    # get the text chunks
+    text_chunks = get_text_chunks(raw_text)
 
-                # create vector store
-                vectorstore = get_vectorstore(text_chunks)
+    # create vector store
+    vectorstore = get_vectorstore(text_chunks)
 
-                # create conversation chain
-                st.session_state.conversation = get_conversation_chain(
-                    vectorstore)
+    # create conversation chain
+    conversation = get_conversation_chain(vectorstore)
 
+    while True:
+        query = input("\nEnter a query: ")
+        if query == "exit":
+            break
+        # Get the answer from the chain
+        response = conversation(query)
+        answer = response["result"]
+
+        # Print the result
+        print("\n\n> Question:")
+        print(query)
+        print("\n> Answer:")
+        print(answer)
 
 if __name__ == '__main__':
     main()
