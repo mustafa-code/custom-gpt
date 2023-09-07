f9af6e31a6b4ed18a90a1fe20ad2cbbd624c3d01
Mustafa El-sir <tommy80080@gmail.com>
2023-09-07 10:28:58 +0300

    Add many changes in api

--- api.py
+++ api.py
@@ -11,39 +11,83 @@ openai.api_key = os.environ["OPENAI_API_KEY"]
 
 app = Flask(__name__)
 
-def sayHi(data):
+def registerStudent(data):
+    print("registerStudent: "+json.dumps(data))
+    return json.dumps({
+        "status": False,
+        "message": "Fail to save student data."
+    })
+
+def askManager(data):
+    print("askManager: "+json.dumps(data))
     return json.dumps({
         "status": True,
-        "message": "Hi "+data["name"]+", enjoy our AI model O_o"
+        "message": "Question have been sent to manager and he will continue the conversation."
     })
 
 def get_func_def():
     return [
         {
-            "name": "sayHi",
-            "description": "A function to greate the user when he write his name",
+            "name": "registerStudent",
+            "description": "Get called when the user provieded lead info",
             "parameters": {
                 "type": "object",
                 "properties": {
                     "name": {
                         "type": "string",
-                        "description": "The user name"
-                    }
+                        "description": "The user's name"
+                    },
+                    "phone": {
+                        "type": "string",
+                        "description": "The user's phone"
+                    },
+                    "email": {
+                        "type": "string",
+                        "description": "The user's email"
+                    },
+                    "programName": {
+                        "type": "string",
+                        "description": "The program\s name student want to apply in"
+                    },
+                    "universityName": {
+                        "type": "string",
+                        "description": "The school\s name student want to apply in"
+                    },
+                    "lang": {
+                        "type": "string",
+                        "description": "The user's conversation language as a lang code like en, ar, or tr"
+                    },
                 },
-                "required": ["name"]
+                "required": ["name", "phone", "email", "programName", "universityName"]
+            }
+        },
+        {
+            "name": "askManager",
+            "description": "Get called when the answer is not known to the model",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "question": {
+                        "type": "string",
+                        "description": "The user's question"
+                    },
+                    "lang": {
+                        "type": "string",
+                        "description": "The conversation language as a lang code like en, ar, or tr"
+                    },
+                },
+                "required": ["question"]
             }
         }
     ]
 
 def callChat(content, chat_id = None, type = "user", function = None):
 
-    if chat_id is None:
-        chat_id = str(uuid.uuid4())
-        file_json = "histories/"+chat_id+'.json'
-        messages = []
-    else :
-        file_json = "histories/"+chat_id+'.json'
+    file_json = "histories/"+chat_id+'.json'
+    if os.path.isfile(file_json):
         messages = json.load(open(file_json))
+    else :
+        messages = []
 
     datasource = ""
     source_documents = []
@@ -54,7 +98,9 @@ def callChat(content, chat_id = None, type = "user", function = None):
             embedding_function=embeddings,
             client_settings=CHROMA_SETTINGS,
         )
-        retriever = db.as_retriever()
+        retriever = db.as_retriever(
+            k=2
+        )
         docs = retriever.get_relevant_documents(content)
         for doc in docs:
             source_documents.append({
@@ -63,12 +109,40 @@ def callChat(content, chat_id = None, type = "user", function = None):
             })
             datasource += doc.page_content + "\n\n"
 
+        source_history_path = "histories/source_docs/"+chat_id+".json"
+        if os.path.isfile(source_history_path):
+            source_history = json.load(open(source_history_path))
+        else :
+            source_history = []
+        source_history.append({
+            "question": content,
+            "source_documents": source_documents,
+        })
+        source_file = open(source_history_path, "w")  
+        json.dump(source_history, source_file)  
+        source_file.close()  
+
+
     messages.insert(0, {
         "role": "system", 
         "content": f"""
-            You must ask the student from his name first and then call sayHi function at once.
+            Use the following pieces of context to answer user question.
+            You are a student assistant to help students apply to OKTamam System.
+            You should answer only to the request and questions related to (learning,universities,Oktamam company), if so apolgaize to the user.
+            Never say you are an AI model, always refer to yourself as a student assistant.
+            If you do not know the answer call askManager Function and send the user question and the language of the conversation to it.
+            Always answer with shortes answer you can, do not say too much.
+            Never say reach out to the university directly or any other similar sentences, instead ask the manager and he will respond to you.
+            It's important to append student language, when calling any functions.
+            If the student wants to register you should ask him for some data one by one in separate questions:
+                - Name
+                - Phone
+                - Email Address
+            when the user give you his/her name, email, and phone number call the registerStudent Function and add user language, program name and university name to the parameters.
+            If there any issue occur then you must call askManager Function and send the question and the language of the conversation.
 
-            {datasource}
+
+            Context: '{datasource}'
         """
     })
 
@@ -81,7 +155,6 @@ def callChat(content, chat_id = None, type = "user", function = None):
         message["name"] = function
 
     messages.append(message)
-
 
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
@@ -99,7 +172,8 @@ def callChat(content, chat_id = None, type = "user", function = None):
 
     if response.get("function_call"):
         available_functions = {
-            "sayHi": sayHi,
+            "registerStudent": registerStudent,
+            "askManager": askManager,
         }
         function_name = response["function_call"]["name"]
         fuction_to_call = available_functions[function_name]
@@ -118,6 +192,9 @@ def prompt_route():
     question = request.form.get("question")
     chat_id = request.form.get("chat_id")
     with_docs = request.form.get("with_docs")
+
+    if chat_id is None:
+        chat_id = str(uuid.uuid4())
 
     response = callChat(question, chat_id)
 
